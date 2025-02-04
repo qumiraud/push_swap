259e78f2365b786da8edcf1427adfaa2f76ed305
Quentin Miraud <qumiraud@2B5.42angouleme.fr>
2025-02-04 15:26:40 +0100

    coder ou decoder, tel est le debug

--- src/over_five.c
+++ src/over_five.c
@@ -6,13 +6,13 @@
 /*   By: qumiraud <qumiraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 09:48:04 by qumiraud          #+#    #+#             */
-/*   Updated: 2025/02/04 12:46:55 by qumiraud         ###   ########.fr       */
+/*   Updated: 2025/02/04 15:24:56 by qumiraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	bestmove_in_top_b(t_stack **stack_b, t_stack **stack_a, int *move)
+void	bestmove_in_top_b_big(t_stack **stack_b, t_stack **stack_a, int *move)
 {
 	t_stack	*nav;
 	int		i;
@@ -24,20 +24,62 @@ void	bestmove_in_top_b(t_stack **stack_b, t_stack **stack_a, int *move)
 		i++;
 		nav = nav->next;
 	}
-	if (i > ft_stacksize(*stack_b)/2 && (*move) >= ft_stacksize(*stack_b)/2)
+	if ((*move) > ft_stacksize(*stack_a)/2)
+		(*move) = ft_stacksize(*stack_a) - (*move);
+	if (i > ft_stacksize(*stack_b)/2 )
 	{
-		while (move > 0 && (*stack_b)->bestmove != 1)
+		i = ft_stacksize(*stack_b) - i;
+		while ((*move) > 0 && i > 0)
 		{
-			reverse_rotate_rotate(stack_b, stack_a);
-			move--;
+			reverse_rotate_rotate(stack_a, stack_b);
+			(*move)--;
+			i--;
+		}
+		while (i > 0)
+		{
+			reverse_rotate_a(stack_b);
+			i--;
+		}
+	}
+	else if (i <= ft_stacksize(*stack_b)/2)
+	{
+		while (i > 0)
+		{
+			rotate_b(stack_b);
+			i--;
+		}
+	}
+
+}
+void	bestmove_in_top_b_little(t_stack **stack_b, t_stack **stack_a, int *move)
+{
+	t_stack	*nav;
+	int		i;
+
+	i = 0;
+	nav = (*stack_b);
+	while (nav->bestmove != 1)
+	{
+		i++;
+		nav = nav->next;
+	}
+
+	if (i > ft_stacksize(*stack_b)/2)
+	{
+		i = ft_stacksize(*stack_b) - i;
+		while ((*move) > 0 && i > 0)
+		{
+			rotate_rotate(stack_b, stack_a);
+			(*move)--;
+			i--;
 		}
 		while ((*stack_b)->bestmove != 1)
-			reverse_rotate_b(stack_b);
-	}
-	if (i <= ft_stacksize(*stack_b)/2)
-	{
-		while ((*stack_b)->bestmove != 1)
 			rotate_b(stack_b);
+	}
+	while (i > 0)
+	{
+		rotate_b(stack_b);
+		i--;
 	}
 }
 
@@ -48,11 +90,13 @@ void	bestmove_in_top_a_big(t_stack **stack_a, t_stack **stack_b, int *move)
 
 	i = 0;
 	nav = (*stack_a);
+	printf ("coucou\n");
 	while (nav->bestmove != 1)
 	{
 		i++;
 		nav = nav->next;
 	}
+	//printf("coucou, i = %d et move = %d\n", i, (*move));
 	if ((*move) > ft_stacksize(*stack_b)/2)
 		(*move) = ft_stacksize(*stack_b) - (*move);
 	if (i > ft_stacksize(*stack_a)/2 )
@@ -129,6 +173,32 @@ void	search_proxi(int ref, t_stack *stack)
 	int			near;
 
 	reset = NULL;
+	near = -2147483648;
+	nav = stack;
+	nav->proxi = 0;
+
+	while (nav)
+	{
+
+		if (nav->nbr < ref && nav->nbr > near)
+		{
+			if (reset)
+				reset->proxi = 0;
+			near = nav->nbr;
+			nav->proxi = 1;
+			reset = nav;
+		}
+		nav = nav->next;
+	}
+}
+
+void	search_proxi_for_return(int ref, t_stack *stack)
+{
+	t_stack		*nav;
+	t_stack		*reset;
+	int			near;
+
+	reset = NULL;
 	near = 2147483647;
 	nav = stack;
 	nav->proxi = 0;
@@ -173,6 +243,57 @@ int	ft_cheapest_move(t_stack **stack_a, t_stack *stack_b)
 		while (nav_b)
 		{
 			search_proxi(nav_a->nbr, stack_b);
+			if (nav_a->nbr > nav_b->nbr  && nav_a->nbr > ft_find_min(nav_b) && nav_b->proxi == 1)
+				break;
+			else if (nav_a->nbr > ft_find_max(nav_b) && nav_b->nbr == ft_find_max(nav_b))
+				break;
+			else if (nav_a->nbr < ft_find_min(nav_b) && nav_b->nbr == ft_find_max(nav_b))
+				break;
+			i++;
+			nav_b = nav_b->next;
+		}
+		nav_b->proxi = 0;
+		if ( i < j)
+		{
+			j = i;
+			if (reset)
+				reset->bestmove = 0;
+			nav_a->bestmove = 1;
+			reset = nav_a;
+			if (j <= 2)
+				return (j);
+		}
+		nav_a = nav_a->next;
+		k++;
+	}
+	return (j);
+}
+
+int	ft_cheapest_move_for_return(t_stack **stack_a, t_stack *stack_b)
+{
+	int		i;
+	int		j;
+	int		size_b;
+	int		k;
+
+	t_stack	*nav_a;
+	t_stack	*nav_b;
+	t_stack	*reset;
+
+	k = 0;
+	nav_a = (*stack_a);
+	nav_b = stack_b;
+	reset = NULL;
+	size_b = ft_stacksize(nav_b);
+	i = 0;
+	j = size_b + ft_stacksize(nav_a) * 2;
+	while (nav_a && k <= size_b / 4)
+	{
+		i = 0;
+		nav_b = stack_b;
+		while (nav_b)
+		{
+			search_proxi_for_return(nav_a->nbr, stack_b);
 			if (nav_a->nbr < nav_b->nbr  && nav_a->nbr > ft_find_min(nav_b) && nav_b->proxi == 1)
 				break;
 			else if (nav_a->nbr > ft_find_max(nav_b) && ft_stacklast(nav_b)->nbr == ft_find_max(nav_b))
@@ -199,6 +320,22 @@ int	ft_cheapest_move(t_stack **stack_a, t_stack *stack_b)
 	return (j);
 }
 
+int	min_posi(t_stack *stack_a)
+{
+	int	i;
+
+	i = 0;
+	while (stack_a && stack_a->nbr != ft_find_min(stack_a))
+	{
+		i++;
+		stack_a = stack_a->next;
+	}
+	if (i > ft_stacksize(stack_a)/2)
+		return (1);
+	else
+		return (0);
+}
+
 void	ft_over_five(t_stack **stack_a, int size_a)
 {
 	t_stack	*stack_b;
@@ -215,6 +352,7 @@ void	ft_over_five(t_stack **stack_a, int size_a)
 	{
 		size_b = ft_stacksize(stack_b);
 		move =ft_cheapest_move(stack_a, stack_b);
+		ft_printstack((*stack_a), stack_b);
 		if (move <= size_b/2)
 		{
 			bestmove_in_top_a_little(stack_a, &stack_b, &move);
@@ -243,10 +381,12 @@ void	ft_over_five(t_stack **stack_a, int size_a)
 	while (ft_stacksize(stack_b) > 0)
 	{
 		size_a = ft_stacksize(*stack_a);
-		move = ft_cheapest_move(&stack_b, (*stack_a));
-		bestmove_in_top_b(&stack_b, stack_a, &move);
+		move = ft_cheapest_move_for_return(&stack_b, (*stack_a));
+
+		//printf("after cheapestmove move value: %d\n", move);
 		if (move <= size_a/2)
 		{
+			bestmove_in_top_b_little(&stack_b, stack_a, &move);
 			while (move > 0 )
 			{
 					rotate_a(stack_a);
@@ -256,6 +396,8 @@ void	ft_over_five(t_stack **stack_a, int size_a)
 		else
 		{
 			move = size_a - (move);
+			//printf("after le after move value: %d\n", move);
+			bestmove_in_top_b_big(&stack_b, stack_a, &move);
 			while (move > 0)
 			{
 				reverse_rotate_a(stack_a);
@@ -264,8 +406,21 @@ void	ft_over_five(t_stack **stack_a, int size_a)
 		}
 		push_a(stack_a, &stack_b);
 	}
-	while ((*stack_a)->nbr != ft_find_min((*stack_a)))
-		rotate_a(stack_a);
+
+	if (min_posi(*stack_a) == 0)
+	{
+		while ((*stack_a)->nbr != ft_find_min((*stack_a)))
+			rotate_a(stack_a);
+	}
+	else
+	{
+		while ((*stack_a)->nbr != ft_find_min((*stack_a)))
+			reverse_rotate_a(stack_a);
+	}
 	//?*********************? go_stack_a ?************************************?//
+	// if (!ft_good_order(*stack_a))
+	// {
+	// 	ft_over_five(stack_a, ft_stacksize(*stack_a));
+	// }
 	return;
 }
